162da03b64ff7e11d69c5b4c9f0a3a895778953e
Ranjeeta Rajput <ranjeeta.rajput@cerence.com>
2021-10-12 10:55:19 +0530

    Added SwiftyLib.podspec

---
+++ SwiftyLib/SwiftyLib.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftyLib.swift
+//  SwiftyLib
+//
+//  Created by Ranjeeta Rajput on 12/10/21.
+//
+
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
